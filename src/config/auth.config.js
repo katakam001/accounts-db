08c646bc58dc98f13530c481637e3e1435f82e39
bkatakam <katakam001@gmail.com>
2025-01-02 18:54:35 +0530

    used for forgetpassword functionality.

--- src/config/auth.config.js
+++ src/config/auth.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   secret: "408601a328ebc7baf849aab5253f3dee0ffaedf7b3134e8304b568460a68ee8fce7ae3ce1a5a279465188c487a250e85a2d13758723137f4fa639333878dad1c",
-  refreshSecret: "7a9f30c54b4b0529fce145c6ee6a36eb2d14ecd7141e25adb97061fe1bfd5bc95cff4b2c39e935d6b8e270f7044a154ee6f4ac8e1336d376cbc622156cfd72f7",
   privateKeyPath: './src/keys/private.pem',
   publicKeyPath: './src/keys/public.pem'
 };
--- src/utils/generateSecrets.js
+++ src/utils/generateSecrets.js
@@ -2,7 +2,5 @@ const crypto = require('crypto');
 
 // Generate a random secret key
 const secret = crypto.randomBytes(64).toString('hex');
-const refreshSecret = crypto.randomBytes(64).toString('hex');
 
 console.log('Secret:', secret);
-console.log('Refresh Secret:', refreshSecret);
