ac7cdb9b0a59b12b1a3c8387fbc03ec411c720f9
bkatakam <katakam001@gmail.com>
2025-09-30 00:43:06 +0530

    removed the unique constraint on transaction_id,is_cash_adjustment,user_id and financial_year on cash_entries_batch table.

--- src/migrations/20250921180912-add-unique-index-cash-entries-and-batch.js
+++ src/migrations/20250921180912-add-unique-index-cash-entries-and-batch.js
@@ -9,16 +9,9 @@ module.exports = {
       fields: ['transaction_id', 'user_id', 'financial_year', 'is_cash_adjustment']
     });
 
-    // Unique index for cash_entries_batch
-    await queryInterface.addIndex('cash_entries_batch', {
-      name: 'cash_entries_batch_unique_txn_user_year_adjustment',
-      unique: true,
-      fields: ['transaction_id', 'user_id', 'financial_year', 'is_cash_adjustment']
-    });
   },
 
   down: async (queryInterface, Sequelize) => {
     await queryInterface.removeIndex('cash_entries', 'cash_entries_unique_txn_user_year_adjustment');
-    await queryInterface.removeIndex('cash_entries_batch', 'cash_entries_batch_unique_txn_user_year_adjustment');
   }
 };
